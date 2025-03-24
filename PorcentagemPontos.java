ac92f7c123d1033261ec6d69b3d81b50ce53e3ae
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2025-03-24 19:43:31 -0300

    Add files via upload

---
+++ PorcentagemPontos.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class PorcentagemPontos {
+
+	public static void main(String[] args) {
+
+int pontuacaoMaxima = 500;
+
+int pontuacaoAtual = 423;
+
+float porcentagemPontuacao = (float) pontuacaoAtual / pontuacaoMaxima * 100.00f;
+System.out.println("A pontuação atual em porcentagem é: " + porcentagemPontuacao);
+
+	}
+
+}
