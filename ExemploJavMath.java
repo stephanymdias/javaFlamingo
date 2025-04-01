b5b377330c666caa1de039df99641ce237bb03aa
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2025-04-01 19:45:09 -0300

    Add files via upload

--- ExemploJavMath.java
+++ ExemploJavMath.java
@@ -10,8 +10,8 @@ public class ExemploJavMath {
 		int numero01 = sc.nextInt();
 		System.out.println("Digite um valor: ");
 		int numero02 = sc.nextInt();
-		System.out.println("O maior valor digitado pelo cliente é o: " + Math.max(numero01, numero02));
-		sc.close();
-		
+		System.out.println("O maior valor digitado pelo cliente é: " + Math.max(numero01, numero02));
+		System.out.println("O menor valor digitado pelo cliente é: " + Math.min(numero01, numero02));
+		sc.close();	
 	}
 }
