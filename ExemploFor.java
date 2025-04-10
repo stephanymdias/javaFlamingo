d938f0cbdc7273f9c8878b8cfa9890fd28ddd494
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2025-04-10 20:38:20 -0300

    Add files via upload

---
+++ ExemploFor.java
@@ -0,0 +1,11 @@
+package exemplos;
+
+public class ExemploFor {
+
+    public static void main(String[]args){
+
+        for(int contadora = 1; contadora < 11; contadora++){
+            System.out.println(contadora + "º" + "BAD - BAMBU AFRICANO DOBRADO");
+        }
+    }
+}
---
+++ ExemploFor2.java
@@ -0,0 +1,14 @@
+package exemplos;
+
+public class ExemploFor2 {
+
+    public static void main(String[] args) {
+        for (int contadora = 1; contadora < 11; contadora++) {
+            for (int contadora01 = 1; contadora01 < 20; contadora01++) {
+                System.out.println("Contadora: " + contadora + " - " + "Contadora01: " + contadora01);
+            }
+            System.out.println(contadora + "º" + "BAD - BAMBU AFRICANO DOBRADO");
+        }
+    }
+}
+
